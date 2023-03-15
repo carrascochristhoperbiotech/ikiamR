a4a7669a7121a770404e1ea740031f5c0cb1f338
carrascochristhoperbiotech <117690680+carrascochristhoperbiotech@users.noreply.github.com>
2023-03-14 20:05:44 -0500

    Initial commit

---
+++ R/plotpuntos.R
@@ -0,0 +1,14 @@
+#' Plot puntos: figura de nube de puntos
+#'
+#' @param x a numeric vector
+#' @param y a numeric vectos
+#'
+#' @return a plot of point
+#' @export
+#' @import ggplot2
+#'
+#' @examples
+#' plotpuntos(c(2,3,4),c(3,5,6))
+plotpuntos <- function(x,y){
+  plot(x,y)
+}
---
+++ R/resumenstats.R
@@ -0,0 +1,15 @@
+#' Resumen exploratorio de datos (Analisi)
+#'
+#' @param x nimeric vector
+#'
+#' @return mean and sd from vector x
+#' @export
+#' @import ggplot2
+#' @import dplyr
+#' @examples
+#' resumen(c(1,2,3,4,8))
+resumen <- function(x){
+  meanx =mean(x, na.rm= TRUE)
+  sdx = sd(x,na.rm = TRUE)
+  print(paste(meanx,sdx))
+}
